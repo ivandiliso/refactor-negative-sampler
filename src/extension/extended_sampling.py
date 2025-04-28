cd4c4ff012d38e7a9febad3b7351a5ca99276544
Ivan Diliso <33879857+ivandiliso@users.noreply.github.com>
2025-04-28 17:19:28 +0200

    [Negative] Initial Relational Sampling Need to change the subset creation procedure BUG poor peformancens with high numerosity of data

--- src/extension/extended_sampling.py
+++ src/extension/extended_sampling.py
@@ -1,33 +1,58 @@
-from typing import Dict, Mapping, Optional, Sequence, Set, Tuple, Union, List, cast
-import torch
-from torch.utils.data import Dataset
+import math
+import pickle
+from abc import ABC, abstractmethod
+from pathlib import Path
+from sys import getsizeof
+from typing import Dict, List, Mapping, Optional, Sequence, Set, Tuple, Union, cast
 
-from pykeen.typing import EntityMapping, MappedTriples, Target
+import torch
+import tqdm as tqdm
+from pykeen.constants import TARGET_TO_INDEX
 from pykeen.sampling import NegativeSampler
 from pykeen.triples import CoreTriplesFactory
-from abc import ABC, abstractmethod
-from pykeen.typing import LongTensor, BoolTensor, MappedTriples
-import math
-from pykeen.constants import TARGET_TO_INDEX
+from pykeen.typing import BoolTensor, EntityMapping, LongTensor, MappedTriples, Target
+from torch.utils.data import Dataset
 
-
-INDEX_TO_TARGET = {v:k for k,v in TARGET_TO_INDEX.items()}
+INDEX_TO_TARGET = {v: k for k, v in TARGET_TO_INDEX.items()}
+SWAP_TARGET = {"head": "tail", "tail": "head"}
+HEAD = 0
+REL = 1
+TAIL = 2
 
 
 class SubSetNegativeSampler(NegativeSampler, ABC):
-    """ Abstract Class Handling static negative sampling, requires implementing
+    """Abstract Class Handling static negative sampling, requires implementing
     a method able to calculate the correct subset pool of negative for each
     entity in the triples set
     """
-    def __init__(self, *, mapped_triples, num_entities = None, num_relations = None, num_negs_per_pos = None, filtered = False, filterer = None, filterer_kwargs = None, **kwargs):
-        super().__init__(mapped_triples=mapped_triples, num_entities=num_entities, num_relations=num_relations, num_negs_per_pos=num_negs_per_pos, filtered=filtered, filterer=filterer, filterer_kwargs=filterer_kwargs)
-        
-        self.subset = self._generate_subset(mapped_triples, **kwargs)
 
+    def __init__(
+        self,
+        *,
+        mapped_triples,
+        num_entities=None,
+        num_relations=None,
+        num_negs_per_pos=None,
+        filtered=False,
+        filterer=None,
+        filterer_kwargs=None,
+        **kwargs,
+    ):
+        super().__init__(
+            mapped_triples=mapped_triples,
+            num_entities=num_entities,
+            num_relations=num_relations,
+            num_negs_per_pos=num_negs_per_pos,
+            filtered=filtered,
+            filterer=filterer,
+            filterer_kwargs=filterer_kwargs,
+        )
+
+        self.subset = self._generate_subset(mapped_triples, **kwargs)
 
     @abstractmethod
     def _corrupt_triple(self, triple: torch.LongTensor, target: Target):
-        """ Corrupt the selected triple using the generated subset. The triple
+        """Corrupt the selected triple using the generated subset. The triple
         has to be corrupted in-place
 
         Args:
@@ -36,19 +61,17 @@ class SubSetNegativeSampler(NegativeSampler, ABC):
         """
         raise NotImplementedError
 
-
     @abstractmethod
     def _generate_subset(self, mapped_triples: MappedTriples, **kwargs):
-        """ Generated the supporting subset to corrupt the triple
+        """Generated the supporting subset to corrupt the triple
 
         Args:
             mapped_triples (MappedTriples): Base triples to generate the subset
         """
         raise NotImplementedError
-    
 
     def corrupt_batch(self, positive_batch: MappedTriples) -> MappedTriples:
-        """ Subset batch corruptor. Uniform corruption between head and tail.
+        """Subset batch corruptor. Uniform corruption between head and tail.
         Corrupts each triple using the generated subset
 
         Args:
@@ -57,15 +80,17 @@ class SubSetNegativeSampler(NegativeSampler, ABC):
         Returns:
             MappedTriples: Batch of negative triples of size (positive_size * num_neg_per_pos, 3)
         """
-        
+
         batch_shape = positive_batch.shape[:-1]
 
         # Clone Negative for corruption (cloned the number of negative per positive )
-        negative_batch = positive_batch.view(-1, 3).repeat_interleave(self.num_negs_per_pos, dim=0)
+        negative_batch = positive_batch.view(-1, 3).repeat_interleave(
+            self.num_negs_per_pos, dim=0
+        )
 
         # Create tarket mask
         target = torch.full((negative_batch.size(0),), fill_value=2)
-        target[:int(negative_batch.size(0)*0.5)] = 0
+        target[: int(negative_batch.size(0) * 0.5)] = 0
         target = target[torch.randperm(target.size(0))]
 
         total_num_negatives = negative_batch.shape[0]
@@ -75,69 +100,123 @@ class SubSetNegativeSampler(NegativeSampler, ABC):
 
         return negative_batch.view(*batch_shape, self.num_negs_per_pos, 3)
 
+    def _dummy_corrupt_triple(self, triple: torch.LongTensor):
+        """Trasform triple in place to a dummy corrupted triple [-1, -1, -1]
+
+        Args:
+            triple (torch.LongTensor): Triple in (h,r,t) format
+        """
+        triple[:] = torch.tensor([-1, -1, -1], device=triple.device, dtype=triple.dtype)
+
+    def _choose_from_pool(self, pool: torch.tensor) -> torch.tensor:
+        """Choose one element from the input pool of elements
+
+        Args:
+            pool (torch.tensor): Pool of elements to choose from
+
+        Returns:
+            torch.tensor: Chosen element in singleton tensor
+        """
+        return pool[torch.randperm(len(pool))[:1]]
 
 
 class CorruptNegativeSampler(SubSetNegativeSampler):
-    """ Negative sampler from "Richard Socher, Danqi Chen, Christopher D Manning, 
-    and Andrew Ng. 2013. Reasoning With Neural Tensor Networks for Knowledge 
+    """Negative sampler from "Richard Socher, Danqi Chen, Christopher D Manning,
+    and Andrew Ng. 2013. Reasoning With Neural Tensor Networks for Knowledge
     Base Completion." Corrupt head and tails based on the subset of entities seen
-    as head or tail of the specific relation 
+    as head or tail of the specific relation
 
     """
-    def __init__(self, *, mapped_triples, num_entities=None, num_relations=None, num_negs_per_pos=None, filtered=False, filterer=None, filterer_kwargs=None):
-        super().__init__(mapped_triples=mapped_triples, num_entities=num_entities, num_relations=num_relations, num_negs_per_pos=num_negs_per_pos, filtered=filtered, filterer=filterer, filterer_kwargs=filterer_kwargs)
-    
+
+    def __init__(
+        self,
+        *,
+        mapped_triples,
+        num_entities=None,
+        num_relations=None,
+        num_negs_per_pos=None,
+        filtered=False,
+        filterer=None,
+        filterer_kwargs=None,
+    ):
+        super().__init__(
+            mapped_triples=mapped_triples,
+            num_entities=num_entities,
+            num_relations=num_relations,
+            num_negs_per_pos=num_negs_per_pos,
+            filtered=filtered,
+            filterer=filterer,
+            filterer_kwargs=filterer_kwargs,
+        )
 
     def _generate_subset(self, mapped_triples):
-        relations = torch.unique(mapped_triples[:,1]).tolist()
+        relations = torch.unique(mapped_triples[:, REL]).tolist()
         subset = dict()
         for r in relations:
-            mask = mapped_triples[mapped_triples[:,1] == r]
+            mask = mapped_triples[mapped_triples[:, REL] == r]
             subset[r] = {
-                "head" : torch.unique(mask[:,0]),
-                "tail" : torch.unique(mask[:,2])
+                "head": torch.unique(mask[:, HEAD]),
+                "tail": torch.unique(mask[:, TAIL]),
             }
         return subset
-    
-    
-    def _corrupt_triple(self, triple: torch.LongTensor, target: Target):
-        rel = int(triple[1])
-        negative_pool = self.subset[rel][target]
-        triple[TARGET_TO_INDEX[target]] = negative_pool[torch.randperm(len(negative_pool))[:1]]
 
+    def _corrupt_triple(self, triple: torch.LongTensor, target: Target):
+        rel = int(triple[REL])
+        negative_pool = self.subset[rel][target]
+        triple[TARGET_TO_INDEX[target]] = self._choose_from_pool(negative_pool)
 
 
 class TypedNegativeSampler(SubSetNegativeSampler):
-    def __init__(self, *, mapped_triples, num_entities=None, num_relations=None, num_negs_per_pos=None, filtered=False, filterer=None, filterer_kwargs=None, relation_domain_range_dict=None, entity_classes_dict=None):
-        super().__init__(mapped_triples=mapped_triples, num_entities=num_entities, num_relations=num_relations, num_negs_per_pos=num_negs_per_pos, filtered=filtered, filterer=filterer, filterer_kwargs=filterer_kwargs, relation_domain_range_dict=relation_domain_range_dict, entity_classes_dict=entity_classes_dict)
+    """Type-Constrained Negative sampler from "Krompaß, D., Baier, S., Tresp, V.: Type-constrained representation
+    learning in knowledge graphs. In: The Semantic Web-ISWC 2015". Produces the subsed of available negatives using only
+    entities that appear as domain (for corruptiong head) and range (for corrupting tails) of a triple relation.
+    Need additional information on triples, a dict with domain and range for each relation (mapped to IDS) and a
+    dictionary of class memebership for each entity (mapped to IDS)
+    """
+
+    def __init__(
+        self,
+        *,
+        mapped_triples,
+        num_entities=None,
+        num_relations=None,
+        num_negs_per_pos=None,
+        filtered=False,
+        filterer=None,
+        filterer_kwargs=None,
+        relation_domain_range_dict=None,
+        entity_classes_dict=None,
+    ):
+        super().__init__(
+            mapped_triples=mapped_triples,
+            num_entities=num_entities,
+            num_relations=num_relations,
+            num_negs_per_pos=num_negs_per_pos,
+            filtered=filtered,
+            filterer=filterer,
+            filterer_kwargs=filterer_kwargs,
+            relation_domain_range_dict=relation_domain_range_dict,
+            entity_classes_dict=entity_classes_dict,
+        )
 
         self.relation_domain_range = relation_domain_range_dict
-        self.mapping = {
-            "head" : "domain",
-            "tail" : "range"
-        }
+        self.mapping = {"head": "domain", "tail": "range"}
 
     def _corrupt_triple(self, triple, target):
-        rel = int(triple[1])
+
+        rel = int(triple[REL])
         target_class = self.relation_domain_range[rel][self.mapping[target]]
-        print(f"Corrupting Triple {triple} on {target}")
-        print(f"Relation {rel} has {target} of class {target_class}")
+
         if target_class != "None":
             negative_pool = self.subset[target_class]
-            print(f"The size of negative pool is {len(negative_pool)}")
-            print(negative_pool)
+
             if len(negative_pool) > 0:
-                print(torch.randperm(len(negative_pool))[:1])
-                triple[TARGET_TO_INDEX[target]] = negative_pool[torch.randperm(len(negative_pool))[:1]]
+                triple[TARGET_TO_INDEX[target]] = self._choose_from_pool(negative_pool)
             else:
-                triple[:] = torch.tensor([-1,-1,-1], device=triple.device, dtype=triple.dtype)
+                self._dummy_corrupt_triple(triple)
         else:
-            triple[:] = torch.tensor([-1,-1,-1], device=triple.device, dtype=triple.dtype)
+            self._dummy_corrupt_triple(triple)
 
-        print(f"New Triple {triple}")
-        print("")
-
-    
     def _generate_subset(self, mapped_triples, **kwargs):
 
         entity_classes = kwargs.get("entity_classes_dict")
@@ -157,10 +236,113 @@ class TypedNegativeSampler(SubSetNegativeSampler):
 
         for class_name, entity_ids in classes_dict.items():
             classes_dict[class_name] = torch.unique(torch.tensor(entity_ids))
-        
+
         return classes_dict
 
-        
 
+class RelationalNegativeSampler(SubSetNegativeSampler):
+    """Relational constrained Negative Sampler from "Kotnis, B., Nastase, V.: Analysis of the impact of negative
+    sampling on link prediction in knowledge graphs".
+    If follows the assuption that each head,tail pair are connected
+    by only one relation, so, fixed the head (tail) we take all the tail (head) elements that appear in the triple with
+    a relation different from the original one.
+    """
 
+    def __init__(
+        self,
+        *,
+        mapped_triples,
+        num_entities=None,
+        num_relations=None,
+        num_negs_per_pos=None,
+        filtered=False,
+        filterer=None,
+        filterer_kwargs=None,
+        local_file=None,
+        **kwargs,
+    ):
 
+        self.local_file = Path(local_file)
+
+        super().__init__(
+            mapped_triples=mapped_triples,
+            num_entities=num_entities,
+            num_relations=num_relations,
+            num_negs_per_pos=num_negs_per_pos,
+            filtered=filtered,
+            filterer=filterer,
+            filterer_kwargs=filterer_kwargs,
+            **kwargs,
+        )
+
+    def _generate_subset(self, mapped_triples, **kwargs):
+
+        subset = dict()
+
+        if self.local_file.is_file():
+            print("[RelationalNegativeSampler] Loading Pre-Computed Subset")
+            with open(self.local_file, "rb") as f:
+                subset = pickle.load(f)
+
+        else:
+            print("[RelationalNegativeSampler] Generating Subset")
+            for entity_id in tqdm.tqdm(range(self.num_entities)):
+
+                entity_subset = {
+                    "head": {
+                        relation_id: dict() for relation_id in range(self.num_relations)
+                    },
+                    "tail": {
+                        relation_id: dict() for relation_id in range(self.num_relations)
+                    },
+                }
+
+                entity_as_head = mapped_triples[mapped_triples[:, HEAD] == entity_id]
+                entity_as_tail = mapped_triples[mapped_triples[:, TAIL] == entity_id]
+
+                for relation_id in range(self.num_relations):
+
+                    # In both cases we take all the relationships different than the current one, as stated in the
+                    # original formualtion
+
+                    # Taking tails connected to the ENTITY_ID as head, for relation that are NOT relation_ID
+                    # This is the set of available tail entity when ENTITY_ID is the HEAD
+                    entity_subset["tail"][relation_id] = entity_as_head[
+                        entity_as_head[:, REL] != relation_id, TAIL
+                    ]
+
+                    # Taking heads connected to the ENTITY_ID as tail, for relation that are NOT relation_ID
+                    # This is the set of available head entity when ENTITY_ID is the TAIL
+                    entity_subset["head"][relation_id] = entity_as_tail[
+                        entity_as_tail[:, REL] != relation_id, HEAD
+                    ]
+
+                subset[entity_id] = entity_subset
+
+            print(getsizeof(subset))
+
+            print(f"[RelationalNegativeSampler] Saved Subset as {self.local_file}")
+
+            with open(self.local_file, "wb") as f:
+                torch.save(subset, f)
+
+        print("MARIOOoo")
+
+        # LRU CACHE FOR FUCTION 
+
+        return subset
+
+    def _corrupt_triple(self, triple, target):
+
+        # If corrupting HEAD we take the TAIL entity to use as a pivot for the subset
+        # If corrupting TAIL we take the HEAD entity to use as a pivot for the subset
+        pivot_entity = triple[TARGET_TO_INDEX[SWAP_TARGET[target]]]
+        rel = triple[REL]
+
+        # Get the subset of element with
+        negative_pool = self.subset[int(pivot_entity)][target][int(rel)]
+
+        if len(negative_pool) > 0:
+            triple[TARGET_TO_INDEX[target]] = self._choose_from_pool(negative_pool)
+        else:
+            self._dummy_corrupt_triple(triple)
--- src/extension/yago_typed_test.py
+++ src/extension/yago_typed_test.py
@@ -12,9 +12,10 @@ from test_utils import *
 from pykeen.pipeline import pipeline
 from pykeen.sampling.filtering import PythonSetFilterer
 from extended_filtering import NullPythonSetFilterer
-from extended_sampling import CorruptNegativeSampler, TypedNegativeSampler
+from extended_sampling import CorruptNegativeSampler, TypedNegativeSampler, RelationalNegativeSampler
 from extended_dataset import OnMemoryDataset
 import torch
+from datetime import datetime
 
 
 # Initial Global Variables Configuration 
@@ -41,24 +42,74 @@ print(tabulate(
 
 # Testing Sampling Techniques
 ################################################################################
-sampler = TypedNegativeSampler(
-    mapped_triples=dataset.training.mapped_triples,
+# sampler = TypedNegativeSampler(
+#     mapped_triples=dataset.training.mapped_triples,
+#     filtered=True,
+#     filterer=NullPythonSetFilterer(mapped_triples=dataset.training.mapped_triples),
+#     num_negs_per_pos = 5,
+#     entity_classes_dict = dataset.entity_id_to_classes,
+#     relation_domain_range_dict = dataset.relation_id_to_domain_range
+# )
+
+
+mapped_triples = torch.tensor([
+    [0, 0, 1],
+    [0, 0, 2],
+    [0, 0, 3],
+    [0, 0, 4],
+    [1, 1, 0],
+    [1, 0, 0],
+    [1, 1, 2],
+    [1, 1, 3],
+    [2, 0, 3],
+    [2, 0, 4]
+])
+
+
+mapped_triples = dataset.training.mapped_triples
+
+
+local_file = Path().cwd() / "relational_save.bin"
+
+
+
+sampler = RelationalNegativeSampler(
+    mapped_triples=mapped_triples,
     filtered=True,
-    filterer=NullPythonSetFilterer(mapped_triples=dataset.training.mapped_triples),
-    num_negs_per_pos = 5,
-    entity_classes_dict = dataset.entity_id_to_classes,
-    relation_domain_range_dict = dataset.relation_id_to_domain_range
+    filterer=NullPythonSetFilterer(
+        mapped_triples=mapped_triples
+    ),
+    num_negs_per_pos=5,
+    local_file=local_file
 )
 
+#print(dataset.relation_to_id)
 
-print(dataset.relation_to_id)
+#negatives = sampler.sample(dataset.training.mapped_triples[torch.randperm(len(dataset.training.mapped_triples))][:10])
 
-negatives = sampler.sample(dataset.training.mapped_triples[torch.randperm(len(dataset.training.mapped_triples))][:10])
+# for k, v in sampler.subset.items():
+#     print("----------------------------------------------")
+    
+#     print(f"For entity {k}")
+#     print(mapped_triples[mapped_triples[:, 0] == k])
+#     print("When it appears as HEAD i can take the following to corrupt its TAIL")
+#     for rel, values in v["tail"].items():
+#         print(f"{rel} {values}")
 
+#     print("")
+#     print(mapped_triples[mapped_triples[:, 2] == k])
+#     print("When it appears as TAIL i can take the following to corrupt its HEAD")
+#     for rel, values in v["head"].items():
+#         print(f"{rel} {values}")
+
+#print(negatives)
+
+print("Generating Negatives")
+
+negatives = sampler.sample(mapped_triples[:10])
 
 
 print(negatives)
-
 
 
 
